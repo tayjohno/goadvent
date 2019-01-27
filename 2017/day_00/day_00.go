f591c82556e77ac75faa7f539eb08b6a4f4d78e0
Taylor Sampson <tayjohno@gmail.com>
2019-01-27 10:32:28 -0600

    Adding template

---
+++ 2017/day_00/day_00.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(PartOne())
+	fmt.Println(PartTwo())
+}
+
+// PartOne Solves the first part of day X of Advent of Code
+func PartOne() int {
+	return 0
+}
+
+// PartTwo Solves the second part of day X of Advent of Code
+func PartTwo() int {
+	return 0
+}
